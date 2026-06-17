7012612c585fdb8cd1c2ad05a21df6bb5807fb84
MuhammadOmarMuhdhar <167825709+MuhammadOmarMuhdhar@users.noreply.github.com>
2026-06-17 07:42:09 +0300

    fix: fresh package-lock.json and add pages deploy workflow

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -6,5 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://plus254.co.ke',
+  base: '/',
   integrations: [mdx(), sitemap()]
 });
