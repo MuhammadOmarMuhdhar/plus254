d8c138ab79b5812bfdd5176dd2f575e44f2bf5e7
MuhammadOmarMuhdhar <167825709+MuhammadOmarMuhdhar@users.noreply.github.com>
2026-07-16 21:01:16 +0300

    Refactoring extractors to pipeline/ - static sources

---
+++ src/plus254/pipeline/extract/static/registered_voters.py
@@ -0,0 +1,17 @@
+import logging
+from pathlib import Path
+import pandas as pd
+import yaml
+from plus254.utils.extractors import pdf
+
+logger = logging.getLogger(__name__)
+
+def extract(datasets: dict | None = None) -> pd.DataFrame:
+
+    slug = Path(__file__).stem
+    
+    if datasets is None:
+        with open('src/plus254/datasets.yaml', 'r') as f:
+            datasets = yaml.safe_load(f)
+    url = datasets[slug]['url']
+    return pdf._get_table(url, pdf_name="Registered Voters per Polling Station")
