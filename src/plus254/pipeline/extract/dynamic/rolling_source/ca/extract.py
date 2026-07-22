00a41ac6c1acc3ba922693bf7e08247d645379f8
MuhammadOmarMuhdhar <167825709+MuhammadOmarMuhdhar@users.noreply.github.com>
2026-07-22 11:30:31 +0300

    Update: updated to use new url pdf downlaoding process

--- src/plus254/pipeline/extract/dynamic/rolling_source/ca/extract.py
+++ src/plus254/pipeline/extract/dynamic/rolling_source/ca/extract.py
@@ -5,8 +5,9 @@ from plus254.utils.extractors import manifest
 logger = logging.getLogger(__name__)
 
 
-def extract():
+def extract(slugs=None):
     return manifest.extract_from_manifest(
         manifest_path=Path(__file__).parent / "manifest.yaml",
         base_url="https://www.ca.go.ke/statistics",
+        slugs=slugs,
     )
